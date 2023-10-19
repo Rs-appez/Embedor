6487ea37d76f45370ab3ec2bb9afdaf55c02f9d1
Rs-appez <alexandrep1999@hotmail.com>
2023-10-19 18:32:33 +0200

    fix

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ function changeVideo() {
 
 	let player = document.getElementById('player');
 	let yt_id = getQueryVariable("v");
-	player.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/'+ yt_id+'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+	player.innerHTML = '<iframe width="100%" height="600" src="https://www.youtube.com/embed/'+ yt_id+'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
 
 }
 function getQueryVariable(variable) {
@@ -17,5 +17,24 @@ function getQueryVariable(variable) {
 }
 }
 
-setTimeout(changeVideo, 2000);
-window.addEventListener('pageshow',changeVideo);
+setTimeout(changeVideo, 1500);
+
+var oldHref = document.location.href;
+
+
+    var bodyList = document.querySelector("body")
+
+    var observer = new MutationObserver(function(mutations) {
+        if (oldHref != document.location.href) {
+            oldHref = document.location.href;
+            setTimeout(changeVideo, 1500);
+        }
+    });
+    
+    var config = {
+        childList: true,
+        subtree: true
+    };
+    
+    observer.observe(bodyList, config);
+
