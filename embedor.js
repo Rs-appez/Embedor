695e999a1c70b8f0964aa4344e3605fab7922f33
Rs-appez <alexandrep1999@hotmail.com>
2023-10-26 10:22:39 +0200

    fix

--- embedor.js
+++ embedor.js
@@ -1,40 +1,46 @@
+function changeVideo() {
 
-function changeVideo() { 
-
-	let player = document.getElementById('player');
-	let yt_id = getQueryVariable("v");
-	player.innerHTML = '<iframe width="100%" height="600" src="https://www.youtube.com/embed/'+ yt_id+'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+    let player = document.getElementById('player');
+    let yt_id = getQueryVariable("v");
+    let sanitized_id = sanitizeString(yt_id);
+    player.innerHTML = '<iframe width="100%" height="600" src="https://www.youtube.com/embed/' + sanitized_id
+        + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
 
 }
 function getQueryVariable(variable) {
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-    if (pair[0] == variable) {
-      return pair[1];
-	}
-}
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) {
+            return pair[1];
+        }
+    }
 }
 
-setTimeout(changeVideo, 1500);
+function sanitizeString(str){
+    str = str.replace(/[^a-z0-9]/gim,"");
+    return str.trim();
+}
+
+setTimeout(changeVideo, 2000);
 
 var oldHref = document.location.href;
 
 
-    var bodyList = document.querySelector("body")
+var bodyList = document.querySelector("body")
 
-    var observer = new MutationObserver(function(mutations) {
-        if (oldHref != document.location.href) {
-            oldHref = document.location.href;
-            setTimeout(changeVideo, 1500);
-        }
-    });
-    
-    var config = {
-        childList: true,
-        subtree: true
-    };
-    
-    observer.observe(bodyList, config);
+var observer = new MutationObserver(function (mutations) {
+    if (oldHref != document.location.href) {
+        oldHref = document.location.href;
+        setTimeout(changeVideo, 1500);
+    }
+});
+
+var config = {
+    childList: true,
+    subtree: true
+};
+
+observer.observe(bodyList, config);
 
